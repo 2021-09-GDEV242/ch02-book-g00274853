afe3096566317feea56d303757f979bef8f34951
g00274853 <70735832+g00274853@users.noreply.github.com>
2021-09-20 03:08:12 -0400

    .88

--- Book.java
+++ Book.java
@@ -12,6 +12,7 @@ class Book
     private String author;
     private String title;
     private int pages;
+    private String refNumber;
 
     /**
      * Set the author and title fields when this object
@@ -22,6 +23,17 @@ class Book
         author = bookAuthor;
         title = bookTitle;
         pages = bookPages;
+        refNumber = "";
+    }
+    
+    public void setRefNumber(String ref)
+    {
+        refNumber = ref;
+    }
+    
+    public String getRefNumber()
+    {
+        return refNumber;
     }
     
     public String getAuthor()
