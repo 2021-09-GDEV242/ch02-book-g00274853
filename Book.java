5a038f1394ab6f3b747fb4734425d5bad4e612ce
g00274853 <70735832+g00274853@users.noreply.github.com>
2021-09-20 03:38:36 -0400

    .92

--- Book.java
+++ Book.java
@@ -14,19 +14,20 @@ class Book
     private int pages;
     private String refNumber;
     private int borrowed;
-
+    private boolean courseText;
     /**
      * Set the author and title fields when this object
      * is constructed.
      */
-    public Book(String bookAuthor, String bookTitle, int bookPages)
+    public Book(String bookAuthor, String bookTitle, int bookPages, boolean isCourseText)
     {
         author = bookAuthor;
         title = bookTitle;
         pages = bookPages;
         refNumber = "";
+        courseText = isCourseText;
     }
-    
+    //set Referance Number
     public void setRefNumber(String ref)
     {
         if(refNumber.length() >= 3)
@@ -39,42 +40,47 @@ class Book
             System.out.println("Reference number at least three!");
         }
     }
-    
+    //This book has been borrowed more and more times
     public String getRefNumber()
     {
         return refNumber;
     }
-    
+    //Author
     public String getAuthor()
     {
         return author;
     }
-    
+    //Title
     public String getTitle()
     {
         return title;
     }
-    
+    //Pages
     public int getPages()
     {
         return pages;
     }
-    
+    //the number of times this book has been borrowed
     public int getBorrowed()
     {
         return borrowed;
     }
-    
+    //Is this book a textbook for a certain course
+    public boolean isCourseText()
+    {
+       return courseText;
+    }
+    //Print out the author 
     public void printAuthor()
     {
         System.out.println("Author" + author);
     }
-    
+    //Print out thr Title
     public void printTitle()
     {
         System.out.println("Title" + title);
     }
-    
+    //Print out Details
     public void printDetails()
     {
         System .out.println("Title" + title + "Aouthor" + author + "Pages" +pages);
@@ -88,7 +94,7 @@ class Book
         }
         System.out.println("Borrowed" + borrowed);
     }
-
+    //The number of times it's borrowed plus 1  
     public void borrowed()
     {
         borrowed++;
